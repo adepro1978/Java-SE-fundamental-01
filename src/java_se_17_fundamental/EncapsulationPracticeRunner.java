799af605df7173455a0ecc2dd54af7365476d23a
Dhaakir <112408429+adepro1978@users.noreply.github.com>
2023-09-10 12:41:10 +0100

    New mathEquation project from CalcEngine project.

---
+++ src/java_se_17_fundamental/EncapsulationPracticeRunner.java
@@ -0,0 +1,13 @@
+package java_se_17_fundamental;
+
+public class EncapsulationPracticeRunner {
+
+        public static void main(String[] args) {
+
+            // create object of Area
+            // pass value of length and breadth
+            EncapsulationPractice rectangle = new EncapsulationPractice(5, 6);
+            rectangle.getEncapsulationPractice();
+
+        }
+    }
